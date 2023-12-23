bc5486df4aab29ad341d20f6b4a8d46201bf9e18
Diego Andrade <contato.andradediego@gmail.com>
2023-12-23 09:34:37 -0300

    Primeiro Push

---
+++ js/app.js
@@ -0,0 +1,50 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (isNaN(qtd) || qtd <=0) {
+        alert ('Por favor, insíra uma quantidade válida.');
+        return;
+    }
+
+    if(tipo.value == 'pista'){
+        comprarPista(qtd);
+    } else if (tipo.value == 'superior'){
+        comprarSuperior(qtd)
+    } else {
+        comprarInferior(qtd)
+    }
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponivel para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponivel para setor superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponivel para setor inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior
+        alert('Compra realizada com sucesso!');
+    }
+}
